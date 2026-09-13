ce276d71bf15f803519070e04469ed471e1ddf0e
roromaniac <rando4ukraine@gmail.com>
2026-09-13 00:08:40 -0400

    type annotation for rsvp

--- app/states/visit_state.py
+++ app/states/visit_state.py
@@ -55,7 +55,7 @@ class VisitState(rx.State):
             print("Error retrieving RSVP count:", e)
 
     @rx.var
-    def get_rsvp_count(self):
+    def get_rsvp_count(self) -> str:
         SCOPES = ["https://www.googleapis.com/auth/forms.responses.readonly"]
 
         raw_creds = os.getenv("SERVICE_ACCOUNT_JSON")
